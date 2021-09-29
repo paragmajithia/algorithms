1552df3926ad426425934a13b39a93f5df79b459
parag.majithia <parag.majithia@trafigura.com>
2021-09-29 14:42:56 +0530

    Leetcode algos -- bit mask

--- java/src/com/leetcode/graph/NumberOfProvinces.java
+++ java/src/com/leetcode/graph/NumberOfProvinces.java
@@ -2,16 +2,7 @@ package com.leetcode.graph;
 
 /**
  * 547. Number of Provinces
- * Medium
- *
- * 3732
- *
- * 203
- *
- * Add to List
- *
- * Share
- * There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+  * There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
  *
  * A province is a group of directly or indirectly connected cities and no other cities outside of the group.
  *
